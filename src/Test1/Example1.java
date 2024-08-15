1d1afca7f4def81757b9f237a154d249bc9feef6
Divyaraounnava <techDvya@gmail.com>
2024-08-14 21:09:12 -0400

    First commit

---
+++ src/Test1/Example1.java
@@ -0,0 +1,19 @@
+package Test1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String hungry = "yes";
+     if( hungry == "yes")
+     
+    	 System.out.println("eat something");
+     
+     else
+     {
+    	 System.out.println("Do work");
+     }
+        
+	}
+
+}
