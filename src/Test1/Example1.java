a584622eb6d7a960286654fe01b0f29b87a66e08
Divyaraounnava <techDvya@gmail.com>
2024-08-16 12:34:30 -0400

    Update Example1.java

--- src/Test1/Example1.java
+++ src/Test1/Example1.java
@@ -4,8 +4,8 @@ public class Example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        String hungry = "yes";
-     if( hungry == "yes")
+        String hungry = "y";
+     if( hungry == "y")
      
     	 System.out.println("eat something");
      
